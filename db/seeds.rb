4f99d6789f4008ebabce6f6afd91964d3b24fd97
Eva Tchidemian <e.tchidemian@yahoo.fr>
2023-02-23 16:40:35 +0100

    list done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# movies.each do |movie|
+#   Movies.create!(title: movie)
+# end
+
+require 'faker'
+
+Movie.destroy_all
+# Créer 10 films aléatoires
+10.times do
+  title = Faker::Movie.title
+  poster_url = Faker::Internet.url
+  rating = Faker::Number.between(from: 1, to: 5)
+  overview = Faker::Lorem.sentences(number: 1)
+
+  Movie.create!(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
